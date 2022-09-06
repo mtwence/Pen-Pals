55d5e1bf94f3591eb5eeb43c0bf7f40e36152e5c
C K <ctkoo@ucsc.edu>
2022-09-05 17:08:59 -0700

    server settings

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,9 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
+
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+
 const userSchema = new Schema({
     username: {
         type: String,
@@ -23,6 +27,21 @@ const userSchema = new Schema({
       },
 
 })
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 const User = model('User', userSchema);
 
