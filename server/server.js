ff19f861bb56f1050248d918a73a54ecc1f9ef15
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-09-07 18:16:03 -0400

    trying stuff

--- server/server.js
+++ server/server.js
@@ -20,6 +20,10 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build'));
+});
+
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
