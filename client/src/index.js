cc21f2fc30b8f367e831fae4e62197d4d00c3761
C K <ctkoo@ucsc.edu>
2022-09-07 22:29:23 -0700

    fonts

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/js/all.js";
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:regular,bold,italic&subset=latin,latin-ext');
 import "./index.css";
 import App from "./App";
 
